0cc2887873f89b185d66cc3c55d5094b182d1b63
Delia <deliatps@outlook.com>
2017-04-26 14:02:59 +0300

    set up project

---
+++ Conference Management System/Conference Management System/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Conference_Management_System.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Conference Management System/Conference Management System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Conference_Management_System.WebApiApplication" Language="C#" %>
