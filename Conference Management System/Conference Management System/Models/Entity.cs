e397ec52c9294efd25c2e4a2658619c4033fc26f
Delia <deliatps@outlook.com>
2017-04-27 12:14:07 +0300

    added generic entity

---
+++ Conference Management System/Conference Management System/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Conference_Management_System.Models
+{
+    public abstract class Entity<T> : IHasId<T>
+    {
+
+        private T id;
+
+        public T Id { get => id; set => id = value; }
+
+    }
+}
