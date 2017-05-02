f7edc18a4c3338d9922164979617d6d4ede70f7d
iuliad217 <iulia.duma@yahoo.com>
2017-05-02 11:22:29 +0300

    added user entity and sequence diagram

---
+++ Conference Management System/Conference Management System/Models/BidValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Conference_Management_System.Models
+{
+    public enum BidValues
+    {
+        ACCEPT,
+        REFUSE, 
+        MAYBE
+    }
+}
