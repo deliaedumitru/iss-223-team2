025fed094170402f726dd9c4c001da5b1ab1633c
ioanacotutiu <ioanacotutiu31@gmail.com>
2017-05-02 11:34:42 +0300

    added recommendation entity and sequence diagram

---
+++ Conference Management System/Conference Management System/Models/Recommendation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecommandationApp
+{
+    public class Recommendation:Entity<int>
+    {
+        private int id;
+        private String text;
+        private int reviewerId;
+        private int submissionId;
+
+        public Recommendation() { }
+
+        public Recommendation(int id,String text, int reviewerId, int submissionId) {
+            this.id = id;
+            this.text = text;
+            this.reviewerId = reviewerId;
+            this.submissionId = submissionId;
+        }
+
+        public String  Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public int ReviewerId
+        {
+            get { return reviewerId; }
+            set { reviewerId = value; }
+        }
+
+        public int SubmissionId
+        {
+            get { return submissionId; }
+            set { submissionId = value; }
+        }
+
+
+    }
+}
