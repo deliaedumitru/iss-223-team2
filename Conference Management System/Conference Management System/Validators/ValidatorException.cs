09af39395ae99e318824e7a6ea879e26aebad322
Vasii <vassy_vass10@yahoo.com>
2017-05-13 16:20:10 +0300

    initial

---
+++ Conference Management System/Conference Management System/Validators/ValidatorException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conference_Management_System.Validators
+{
+    class ValidatorException : SystemException
+    {
+        public ValidatorException(string message) : base(message) { }
+    }
+}
