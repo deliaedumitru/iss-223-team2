defaf6889e29099fc3365ccf51195c51cb0a7457
andreeafilipciuc <filipciucAndreeaa@yahoo.com>
2017-05-15 23:18:16 +0300

    added SignupController

--- Conference Management System/Conference Management System/Controllers/SignupController.cs
+++ Conference Management System/Conference Management System/Controllers/SignupController.cs
@@ -5,20 +5,20 @@ using System.Web;
 
 using Conference_Management_System.Repositories;
 using Conference_Management_System.Models;
-using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace Conference_Management_System.Controllers
 {
-    public class SingupController
+    public class SignupController:Controller
     {
         ICrudRepository<String,User> repo;
 
-        public SingupController()
+        public SignupController()
         {
             //repo = new UserRepository();
         }
 
-        public SingupController(ICrudRepository<String,User> repo)
+        public SignupController(ICrudRepository<String,User> repo)
         {
             this.repo = repo;
         }
@@ -30,55 +30,13 @@ namespace Conference_Management_System.Controllers
         * </summary>
         * <param name="user">the User to add</param>
         */
-        public void Add(User user)
+        [HttpPost]
+        public ActionResult Add(User user)
         {
             repo.Add(user);
+            //repo.Save();
+            return Redirect("/");
         }
 
-
-        /*<summary>
-        * Delete the User with the given id
-        * Raises a RespositoryException if the operation fails(the id doesn't exist)
-        * </summary>
-        * <param name="id">the id of the User to remove</param>
-        */
-        public void Delete(string id)
-        {
-            repo.Delete(id);
-        }
-
-
-        /* <summary>
-        * Update a given User
-        * Raises a RespositoryException if the operation fails(ie. the give id doesn't exist)
-        * </summary>
-        * <param name="user"> The new value for the User </param>
-        */
-        public void Update(User user)
-        {
-            repo.Update(user);
-        }
-
-
-        /*<summary>
-        * Gets all the Users of the repository
-        * </summary>
-        * Returns : A queryable(ie. list) of all the the items in the repo 
-        */
-        public IQueryable<User> FindAll()
-        {
-            return repo.FindAll();
-        }
-
-
-        /*<summary>
-        * Gets all the Users of the repository fulfilling a given predicate
-        * </summary>
-        * <returns> A queryable(ie. list) of all the the items in the repo </returns> 
-        */
-        public IQueryable<User> FindBy(Expression<Func<User, bool>> predicate)
-        {
-            return repo.FindBy(predicate);
-        }
     }
 }
