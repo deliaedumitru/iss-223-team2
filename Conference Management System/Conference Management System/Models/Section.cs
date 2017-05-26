ad02750b4a625411e17cd01b454bfa34d4702c33
octi2011 <octi2011@yahoo.com>
2017-05-26 20:15:08 +0300

    Added Section entity

---
+++ Conference Management System/Conference Management System/Models/Section.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Conference_Management_System.Models
+{
+    public class Section : Entity<int>
+    {
+        public Section()
+        {
+
+        }
+
+        public Section(int id, String name, String room, int seatNumber, Conference conference)
+        {
+            Id = id;
+            Name = name;
+            Room = room;
+            SeatNumber = seatNumber;
+            Conference = conference;
+        }
+
+        public String Name { get; set; }
+        public String Room { get; set; }
+        public int SeatNumber { get; set; }
+
+        public virtual Conference Conference;
+        
+        public virtual List<User> Listeners
+        { get; set; }
+
+        public virtual List<User> Speakers
+        { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Room: {1}, SeatNumber: {2}, Conference: {3}", Name, Room, SeatNumber, Conference.Name);
+        }
+
+    }
+}
