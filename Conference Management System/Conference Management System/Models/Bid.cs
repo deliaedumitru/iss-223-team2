340621e93071b651d1c598784c84107dea112c9e
alexandradocolin <aly_docolin@yahoo.com>
2017-05-27 16:27:05 +0300

    Update Bid.cs

--- Conference Management System/Conference Management System/Models/Bid.cs
+++ Conference Management System/Conference Management System/Models/Bid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,14 +7,19 @@ namespace Conference_Management_System.Models
 {
     public class Bid : Entity<int>
     {
+        int id;
+        BidValues value;
+
         public Bid()
         {
         }
 
-        public Bid(int id)
+        public Bid(int id, BidValues value)
         {
             base.Id = id;
+            this.value = value;
         }
+        public BidValues Value { get; set; }
 
         public virtual User Reviewer { get; set; }
         public virtual Submission Submission { get; set; }
