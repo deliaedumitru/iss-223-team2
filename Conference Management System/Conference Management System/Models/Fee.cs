efc33e97e7aec04fea5320005c02744e9d703c51
ghergheldenisa <ghergheldenisa@gmail.com>
2017-06-06 21:02:08 +0300

    added fee model + coresponding table in db

---
+++ Conference Management System/Conference Management System/Models/Fee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Conference_Management_System.Models
+{
+    public class Fee : Entity<int>
+    {
+        public Fee() { }
+
+        public Fee(FeeType type, User user)
+        {
+            this.Type = type;
+            this.User = user;
+        }
+
+        public FeeType Type { get; set; }
+        public virtual User User { get; set; }
+    }
+}
