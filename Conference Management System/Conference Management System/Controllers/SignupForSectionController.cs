e737f9569bbdc25b0d6ad640bc00a252da4cfb8e
andreeafilipciuc <filipciucAndreeaa@yahoo.com>
2017-06-07 22:36:48 +0300

    added sign up for section feature

--- Conference Management System/Conference Management System/Controllers/SignupForSectionController.cs
+++ Conference Management System/Conference Management System/Controllers/SignupForSectionController.cs
@@ -15,20 +15,36 @@ namespace Conference_Management_System.Controllers
         [HttpGet]
         public ActionResult GetAllSections()
         {
+            List<Section> allSections = new List<Section>();
             List<Section> sections;
+            List<Section> userSections = new List<Section>();
             using (var context = new CMS())
             {
                 var sectionRepo = new AbstractCrudRepo<int, Section>(context);
-
+                int userId = Int32.Parse(Request.Cookies["user"]["id"]);
                 // get all the sections
                 sections = sectionRepo.FindAll().ToList();
+                bool a = false;
                 foreach(Section section in sections)
                 {
+                    a = false;
                     section.Conference = section.Conference;
+                    section.Listeners = section.Listeners;
+                    foreach (User user in section.Listeners)
+                        if (user.Id == userId)
+                        {
+                            userSections.Add(section);
+                            a = true;
+                        }
+                        
+                    if(a==false)
+                        allSections.Add(section);
+
                 }
             }
 
-            return View(sections);
+            SectionsListeners sl = new SectionsListeners(allSections, userSections);
+            return View(sl);
         }
 
 
@@ -70,6 +86,49 @@ namespace Conference_Management_System.Controllers
                 }
             }
           
+            return RedirectToAction("GetAllSections");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int sectionId)
+        {
+            List<Section> sections;
+            using (var context = new CMS())
+            {
+                var sectionRepo = new AbstractCrudRepo<int, Section>(context);
+                var userRepo = new AbstractCrudRepo<int, User>(context);
+
+                User listener = null;
+                //Get the user id from the session
+                foreach (User user in userRepo.FindAll().ToList())
+                {
+                    if (user.Id == Int32.Parse(Request.Cookies["user"]["id"]))
+                        listener = user;
+                }
+
+
+                // delete the user to the section selected
+                sections = sectionRepo.FindAll().ToList();
+                foreach (Section sec in sections)
+                {
+                    if (sec.Id == sectionId)
+                    {
+                        List<User> listeners;
+                        if (sec.Listeners.Count == 0)
+                        {
+                            listeners = new List<User>();
+                        }
+                        else 
+                            listeners = sec.Listeners;
+
+                        listeners.Remove(listener);
+                        sec.Listeners = listeners;
+                        sectionRepo.Update(sec);
+                        sectionRepo.Save();
+                    }
+                }
+            }
+
             return RedirectToAction("GetAllSections");
         }
     }
