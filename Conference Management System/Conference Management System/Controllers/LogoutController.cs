88fff98bc40732118eb4a05bdd3d695841f4ab19
ghergheldenisa <ghergheldenisa@gmail.com>
2017-06-08 22:11:00 +0300

    added logout feature + fixed bugs with pay fee

---
+++ Conference Management System/Conference Management System/Controllers/LogoutController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Data.Linq;
+using Conference_Management_System.Repositories;
+using Conference_Management_System.Models;
+
+namespace Conference_Management_System.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public ActionResult Logout()
+        {
+            try
+            {
+                Response.Cookies["user"]["username"] = null;
+                Response.Cookies["user"]["id"] = null;
+                Response.Cookies["user"]["role"] = null;
+                Response.Cookies["user"].Expires = DateTime.Now.Subtract(TimeSpan.FromDays(2));
+
+                String toRedirect = "/Login/FindUserBy";
+                Response.Redirect(toRedirect);
+                return null;
+            }
+            catch (Exception)
+            {
+                String toRedirect = "/Login/FindUserBy";
+                Response.Redirect(toRedirect);
+                return null;
+            }
+        }
+    }
+}
